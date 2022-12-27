4a522106cab03ca8f7066c9f06f9f2ea411a39f9
Matthew Lee <matt.lee.hr@gmail.com>
2022-12-27 09:30:03 -0800

    added basic layout of website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Body">
+      <div className="Sidebar">
+        <p>Hello World!</p>
+      </div>
+      <div className="Calendar">
+        <p>Calendar!</p>
+      </div>
     </div>
   );
 }
