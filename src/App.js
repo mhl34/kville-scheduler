484f59fefa75c12837498354639ecfa4a4ed22e2
Matthew Lee <matt.lee.hr@gmail.com>
2022-12-28 10:23:26 -0800

    weekly calendar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import './App.css';
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 function App() {
   const [file, setFile] = useState();
 
   const handleUpload = () => {
-    if (!file) {
+    /*
+    const files = document.getElementById("fileList").files;
+    let fileReader = new FileReader();
+
+    if (!files) {
       return;
     }
-
+    var fileIcs = {}
+    console.log(files)
+    for (let i = 0; i < files.length; i++) {
+      Object.assign(fileIcs, files[i])
+    }
+    setFile(fileIcs);
+    console.log(file);
+    alert("File Upload Successful!");
+    */
+    const files = document.getElementById("fileList").files;
+    setFile(files[0]);
   }
 
   return (
     <div className="Body">
       <div className="Sidebar">
         <b>Input your calendars here!</b>
-        <input type="file" accept=".ics" />
+        <input id="fileList" type="file" accept=".ics" multiple />
         <button onClick={handleUpload}>Upload</button>
       </div>
       <div className="Calendar">
         <b>Calendar!</b>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridWeek"
+        />
       </div>
     </div>
   );
